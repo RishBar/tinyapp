dd9b43466f47c43e3b118ae165263da7a55cd913
Rishav Baral <rishavbaral6@gmail.com>
2020-06-10 12:22:36 +0000

    add route to /register path

--- express_server.js
+++ express_server.js
@@ -60,6 +60,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get('/register', (req, res) => {
+  res.render("register");
+});
+
 app.get("/urls/new", (req, res) => {
   let cookies = {
     username: req.cookies["username"]
